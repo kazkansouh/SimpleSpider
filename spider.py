a5ac0026ee76b573cf9a0c43c6cf034eef74fc8e
Karim Kanso <kaz.kanso@gmail.com>
2021-02-28 11:45:14 +0000

    bump year

--- spider.py
+++ spider.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright (C) 2020 Karim Kanso
+# Copyright (C) 2021 Karim Kanso
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
